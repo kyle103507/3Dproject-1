5fd47558b48e1d45d0c720ddc1c652a9fb8a1719
kyle103507 <61506510+kyle103507@users.noreply.github.com>
2020-10-11 18:03:20 +0800

    Initial commit

---
+++ Assets/MH044/MH004Anim.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MH004Anim : MonoBehaviour
+{
+    [Header("撞暈時間")] [SerializeField] float StunnedTime = 1;
+    [Header("旋轉碰撞半徑")] [SerializeField] float RotateRadius = 1.5f;
+    [Header("旋轉碰撞圖層")] [SerializeField] LayerMask RotateLayer;
+    [Header("旋轉的力道")] [SerializeField] float RotateForce = 500;
+    Animator Anim;
+
+    private void Start()
+    {
+        // Anim 得到動畫元件
+        Anim = GetComponent<Animator>();
+    }
+
+    public void SetSpeed(float Speed)
+    {
+        // 如果沒有按下Shift鍵速度為走路速度
+        Speed = Mathf.Clamp(Speed, 0, .5f);
+
+        // 如果按下Shift鍵 速度改為 跑步速度
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            Speed += .5f;
+
+        // 執行跑步或走路動畫
+        Anim.SetFloat("Speed", Speed);
+    }
+
+    // 撞暈行為
+    public void StunnedBehavior()
+    {
+        // 開啟暈眩動畫
+        Anim.SetTrigger("Stun");
+    }
+
+    // 暈眩事件
+    public void StunnedEvent()
+    {
+        StartCoroutine(Recoverydelay(StunnedTime));
+    }
+
+    IEnumerator Recoverydelay(float time)
+    {
+        // 得到 HM004Move腳本
+        MH004Move MoveElement = GetComponent<MH004Move>();
+        // 關閉 HM004Move腳本
+        MoveElement.enabled = false;
+        // 關閉 HM004Anim腳本
+        enabled = false;
+
+        // 等待 time秒 後執行下面
+        yield return new WaitForSeconds(time);
+
+        // 開啟 HM004Move腳本
+        MoveElement.enabled = true;
+        // 開啟 HM004Anim腳本
+        enabled = true;
+    }
+
+    // 旋轉動畫
+    public void RotateBehavior(bool IsRotate)
+    {
+        // 如果IsRotate是true 開啟旋轉  不是時候 關閉旋轉
+        Anim.SetBool("Rotate", IsRotate);
+    }
+    // 旋轉功能
+    public void RotateEvent()
+    {
+        // 得到角色附近是RotateLayer圖層的碰撞器
+        Collider[] collision = Physics.OverlapSphere(transform.position, RotateRadius, RotateLayer);
+
+        // 跑過每個附近 RotateLayer圖層的碰撞器
+        foreach (Collider i in collision)
+        {
+            // 得到RotateLayer圖層的碰撞器 的鋼體
+            Rigidbody rb = i.GetComponent<Rigidbody>();
+            // 如果他有剛體就執行碰撞
+            if(rb != null)
+            {
+                rb.AddExplosionForce(RotateForce, transform.position, RotateRadius);
+            }
+        }
+    }
+
+    // 畫線顯示自訂碰撞範圍黃色參考輔助線
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawWireSphere(transform.position, RotateRadius);
+    }
+}
---
+++ Assets/MH044/MH004Move.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MH004Move : MonoBehaviour
+{
+    [SerializeField] float WalkSpeed = 3;
+    [SerializeField] float RunSpeed = 6;
+    [SerializeField] LayerMask GroundLayer;
+    float ForwardDirection;
+    float allowPlayerRotation = .1f;
+
+    Camera cam;
+    CharacterController Controller;
+    MH004Anim anim;
+
+    Vector3 DesiredMoveDirection;
+    float DesiredRotationSpeed = .1f;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        Controller = GetComponent<CharacterController>();
+        anim = GetComponent<MH004Anim>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            anim.RotateBehavior(true);
+        else if (Input.GetMouseButtonUp(0))
+            anim.RotateBehavior(false);
+
+        MoveBehavior();
+    }
+
+    void MoveBehavior()
+    {
+        float InputX = Input.GetAxis("Horizontal");
+        float InputZ = Input.GetAxis("Vertical");
+        // 計算 InputX + InputZ 的合力
+        ForwardDirection = new Vector2(InputX, InputZ).sqrMagnitude;
+
+        anim.SetSpeed(ForwardDirection);
+
+        if (ForwardDirection > allowPlayerRotation)
+        {
+            Vector3 camForward = cam.transform.forward;
+            Vector3 camRight = cam.transform.right;
+
+            camForward.y = 0;
+            camRight.y = 0;
+
+            camForward.Normalize();
+            camRight.Normalize();
+
+            DesiredMoveDirection = camForward * InputZ + camRight * InputX;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(DesiredMoveDirection), DesiredRotationSpeed);
+
+            float Speed = SpeedJudgment();
+
+            if (Physics.Raycast(transform.position, Vector3.down, .1f, GroundLayer))
+                DesiredMoveDirection.y = 0;
+            else
+                DesiredMoveDirection.y = -2;
+            Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
+
+            Controller.Move(DesiredMoveDirection * Time.deltaTime * Speed);
+        }
+    }
+
+    // 速度判斷
+    float SpeedJudgment()
+    {
+        float m_Speed;
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.LeftShift))
+            m_Speed = RunSpeed;
+        else
+            m_Speed = WalkSpeed;
+
+        return m_Speed;
+    }
+}
